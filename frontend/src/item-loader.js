3e556eb8cbfed887ea90663f922e0865e7a04d29
Guillermo Jiménez Díaz <gjimenez@ucm.es>
2023-11-22 16:02:35 +0100

    Instalado handlebars como template engine. Primeras pruebas de uso del template engine

--- frontend/src/item-loader.js
+++ frontend/src/item-loader.js
@@ -1,133 +1,97 @@
-export const anArtworkRow = {
-    "_id": "634f19096c70ef5cf152f237",
-    "tittle": "Scandia",
-    "Object": "CUTLERY",
-    "Special name": "*",
-    "id": "44171",
-    "author": "Franck, Kaj",
-    "Production date": "1952",
-    "Collection": "K\u00c3\u00a4ytt\u00c3\u00b6kokoelma",
-    "Manufacturer": "Hackman Sorsakoski",
-    "Dimension in cm": "fork: 17.5 cm (length) x 0.15cm (thickness)  knife: 18.6 cm (length) x 0.3/0.15 cm (thickness)  spoon: 18 cm (length)",
-    "Weight in kg": "*",
-    "Materials": [
-        "metal"
-    ],
-    "Colour": [
-        "metal"
-    ],
-    "_datasetid": "0daa0287-d7f4-4f03-a068-95f43afcc347",
-    "_timestamp": 1668599056,
-    "_timestamp_year": 2022,
-    "_timestamp_month": 11,
-    "_timestamp_day": 16,
-    "_timestamp_hour": 11,
-    "_timestamp_minute": 44,
-    "_timestamp_second": 16,
-    "_updated": 1.0,
-    "year": 1952.0,
-    "ColourRGB": [
-        [
-            "109",
-            "114",
-            "120"
-        ]
-    ],
-    "image": "https://i.postimg.cc/Zqn1M35m/44171.png"
-};
+import Handlebars from "handlebars";
+const itemTemplate = `
+    <div class="item">
+        <h1>{{_id}}</h1>
+        <div class="item-desc-table">
+            <table>
+            <tbody>
+            <tr>
+                <td class="name">Title</td>
+                <td class="value">{{tittle}}</td>
+            </tr>
+            <tr>
+            {{#if image}}
+                <td class="name">image</td>
+                <td class="value"><img src="{{image}}"/></td>
+            {{/if}}
+            </tr>
+            </tbody>
+            </table>
+        </div>
+    </div>
+`;
 
-
-export const anArtworkCol = {
-    "_id": "634f19119d480d2bcc4ac7ac",
-    "tittle": "Suopursuja (Marsh Tea)",
-    "Object": "WALL TEXTILE",
-    "Special name": "ryijy rug",
-    "id": "16851",
-    "author": "Raitio, A. W.",
-    "Production date": "1934",
-    "Collection": "*",
-    "Manufacturer": "Oy Neovius Ab",
-    "Dimension in cm": "89 cm x 137 cm",
-    "Weight in kg": "*",
-    "Materials": [
-        "wool",
-        "cotton"
-    ],
-    "Colour": [
-        "brown",
-        " light pastel shade"
-    ],
-    "_datasetid": "0daa0287-d7f4-4f03-a068-95f43afcc347",
-    "_timestamp": 1666128145,
-    "_timestamp_year": 2022,
-    "_timestamp_month": 10,
-    "_timestamp_day": 18,
-    "_timestamp_hour": 22,
-    "_timestamp_minute": 22,
-    "_timestamp_second": 25,
-    "_updated": null,
-    "year": 1934.0,
-    "ColourRGB": [
-        [
-            "165",
-            "42",
-            "42"
-        ]
-    ],
-    "image": "https://i.postimg.cc/rppk23gv/16851.jpg",
-    "Object group": "wall textiles"
-};
-
-export function createItemElement(anArtWork){
-    let attribs = ["id", "tittle", "Object", "author", "year", "Materials", "Colour", "ColourRGB", "image", "Object group"];
-    let bodyNode = document.createElement('tbody');
-    bodyNode.classList.add("artwork");
-
-    for (let att of attribs) {
-        if (att === "ColourRGB") {
-            continue;
-        }
-        let rowNode = document.createElement('tr');
-        let nameNode = document.createElement('td');
-        nameNode.classList.add("name");
-        nameNode.innerHTML = att;
-        let valueNode = document.createElement('td');
-        valueNode.classList.add("value");
-
-        if (att === "Colour") {
-            let i = 0;
-            for (let col of anArtWork[att]) {
-                let squareColourNode = document.createElement('div');
-                squareColourNode.classList.add("box");
-                valueNode.appendChild(squareColourNode);
-                let colour = `rgb(${anArtWork["ColourRGB"][i]})`;
-                squareColourNode.style['background-color'] = colour;
-                valueNode.appendChild(squareColourNode);
-                let colourText = document.createTextNode(col);
-                valueNode.appendChild(colourText);
-                i++;
-            }
-        }
-
-        else if (att === "image") {
-            let imageNode = document.createElement("img");
-            imageNode.setAttribute("src", anArtWork[att]);
-            valueNode.appendChild(imageNode);
-        }
-        else {
-            valueNode.innerHTML = anArtWork[att];
-        }
-
-        rowNode.appendChild(nameNode);
-        rowNode.appendChild(valueNode);
-
-        bodyNode.appendChild(rowNode);
+class ItemLoader {
+    _changeItem(item, selector) {
+        let itemElement = this._createItemElement(item);
+        let colElement = document.querySelector(selector);
+        colElement.replaceWith(itemElement);
     }
 
-    let artworkNode = document.createElement('table');
-    artworkNode.classList.add("table");
-    artworkNode.classList.add("table-striped");
-    artworkNode.appendChild(bodyNode);
-    return artworkNode;
-} 
+    _createHandlebars(anArtWork) {
+        let template = Handlebars.compile(itemTemplate);
+        console.log("Compiled template");
+        console.log(template(anArtWork));
+    }
+    _createItemElement(anArtWork) {
+        let attribs = ["id", "tittle", "Object", "author", "year", "Materials", "Colour", "ColourRGB", "image", "Object group"];
+        let bodyNode = document.createElement('tbody');
+        bodyNode.classList.add("artwork");
 
+        for (let att of attribs) {
+            if (att === "ColourRGB") {
+                continue;
+            }
+            let rowNode = document.createElement('tr');
+            let nameNode = document.createElement('td');
+            nameNode.classList.add("name");
+            nameNode.innerHTML = att;
+            let valueNode = document.createElement('td');
+            valueNode.classList.add("value");
+
+            if (att === "Colour") {
+                let i = 0;
+                for (let col of anArtWork[att]) {
+                    let squareColourNode = document.createElement('div');
+                    squareColourNode.classList.add("box");
+                    valueNode.appendChild(squareColourNode);
+                    let colour = `rgb(${anArtWork["ColourRGB"][i]})`;
+                    squareColourNode.style['background-color'] = colour;
+                    valueNode.appendChild(squareColourNode);
+                    let colourText = document.createTextNode(col);
+                    valueNode.appendChild(colourText);
+                    i++;
+                }
+            }
+
+            else if (att === "image") {
+                let imageNode = document.createElement("img");
+                imageNode.setAttribute("src", anArtWork[att]);
+                valueNode.appendChild(imageNode);
+            }
+            else {
+                valueNode.innerHTML = anArtWork[att];
+            }
+
+            rowNode.appendChild(nameNode);
+            rowNode.appendChild(valueNode);
+
+            bodyNode.appendChild(rowNode);
+        }
+
+        let artworkNode = document.createElement('table');
+        artworkNode.classList.add("table");
+        artworkNode.classList.add("table-striped");
+        artworkNode.appendChild(bodyNode);
+        return artworkNode;
+    } 
+
+    changeRowItem(item) {
+        this._changeItem(item, '#item-row table');
+    }
+
+    changeColItem(item) {
+        this._changeItem(item, '#item-col table');
+    }
+}
+export const itemLoader = new ItemLoader();
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,6 @@
 import Plotly from 'plotly.js-dist-min';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { anArtworkRow, anArtworkCol, createItemElement } from './item-loader';
+import {  itemLoader } from './item-loader';
 
 import { daoItems } from './mockdata';
 
@@ -16,18 +16,6 @@ for (let i = 0; i<itemSize; i++) {
     }
     mockSimData[i] = row;
 }
-
-let xValues = ['A', 'B', 'C', 'D', 'E'];
-
-let yValues = ['W', 'X', 'Y', 'Z'];
-
-let zValues = [
-    [0.00, 0.00, 0.75, 0.25, 0.00],
-    [0.00, 0.00, 0.75, 0.75, 0.00],
-    [0.75, 0.75, 0.5, 0.75, 0.75],
-    [0.00, 0.00, 0.00, 0.75, 0.00]
-];
-
 
 const colorscaleValue = [
     [0, '#3D9970'],
@@ -69,28 +57,9 @@ window.addEventListener("load", (event) => {
        if (data.points.length === 1){
             let row = data.points[0].x;
             let col = data.points[0].y;
-            changeRowItem(daoItems.getItemById(row));
-            changeColItem(daoItems.getItemById(col));
+            itemLoader.changeRowItem(daoItems.getItemById(row));
+            itemLoader.changeColItem(daoItems.getItemById(col));
+           itemLoader._createHandlebars(daoItems.getItemById(col));
         }
     });
-
-
-    changeItem(anArtworkCol, '#item-col .item-desc-table');
-    changeItem(anArtworkRow, '#item-row .item-desc-table');
-
 })
-
-function changeItem(item, selector) {
-    let itemElement = createItemElement(item);
-    let colElement = document.querySelector(selector);
-    colElement.appendChild(itemElement);
-}
-
-function changeRowItem(item){
-    changeItem(item, '#item-row .item-desc-table');
-}
-
-function changeColItem(item) {
-    changeItem(item, '#item-col .item-desc-table');
-}
-
