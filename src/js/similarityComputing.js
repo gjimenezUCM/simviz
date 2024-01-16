c287092446b8cc1cbf49ead1102e5c7cb7dba04c
Guillermo Jimenez Diaz <gjimenez@ucm.es>
2024-01-16 14:12:49 +0100

    Computing a new similarity with UI
    Created a class to recompute the global similarity values after modifying the weights. It only works on the attributes with a local similarity value

---
+++ src/js/similarityComputing.js
@@ -0,0 +1,30 @@
+export class SimilarityComputing {
+    constructor(oldSimilarity, newSimilarityDescription){
+        this.oldSimilarity = oldSimilarity
+        this.newSimilarityDescription = newSimilarityDescription
+    }
+
+    run(){
+        let oldSimilarityData = this.oldSimilarity.similarityValues;
+        this.newSimilarityData = [];
+        for (let simPair of oldSimilarityData) {
+            let newSimPair = Object.assign({}, simPair);
+            let newSimValue = 0.0;
+            let totalWeight = 0.0;
+            for (let [att,localFunc] of Object.entries(this.newSimilarityDescription.localSim)) {
+                let weight = localFunc.weight
+                totalWeight+=weight;
+                if (att in simPair['value']['local']){
+                    newSimValue += weight * simPair['value']['local'][att];
+                }
+            }
+            if (totalWeight!==0){
+                newSimValue/=totalWeight;
+            } else {
+                newSimValue = 0.0;
+            }
+            newSimPair['value']['global'] = newSimValue;
+            this.newSimilarityData.push(newSimPair);
+        }
+    }
+}
