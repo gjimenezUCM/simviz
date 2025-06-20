812d38d4ce13aa46267b3fa5d2d8cf8749a77487
Guillermo Jiménez Díaz <gjimenez@ucm.es>
2025-06-20 10:24:09 +0200

    Finishing placeholder version of tabulator
    Datatable.html contains a complete exampls about how to migrate to tabulator. We developed renderers for every datatype currently supported by simviz

--- src/js/datatable.js
+++ src/js/datatable.js
@@ -15,65 +15,109 @@ import nanoMarkdown from 'nano-markdown';
 
 import { FormatModule, DataTreeModule, Tabulator } from 'tabulator-tables';
 
-const data =[ {
+const data = [
+  {
     attribute: {
-        name: "year",
-        type: "number",
-        similarityConfiguration: {
-            "simFunction": "RangeSimilarity",
-            "weight": 0.5*100,
-            "description": "Similarity normalized in range (1950,2021)"
-        },
+      name: "year",
+      type: "number",
+      similarityConfiguration: {
+        simFunction: "RangeSimilarity",
+        weight: 0.5 * 100,
+        description: "Similarity normalized in range (1950,2021)",
+      },
     },
     leftCase: 2016,
     rightCase: 2018,
     value: 0.972,
-},
-    {
+  },
+  {
+    attribute: {
+      name: "image",
+      type: "Image",
+      similarityConfiguration: {
+        simFunction: "lorem ipsum",
+        weight: 0.1 * 100,
+        description: "lorem ipsum",
+      },
+    },
+    leftCase: "https://i.postimg.cc/Zqn1M35m/44171.png",
+    rightCase: "",
+    value: 0.05,
+  },
+
+  {
+    attribute: {
+      name: "paint_color",
+      type: "Color",
+      similarityConfiguration: {
+        simFunction: "lorem ipsum",
+        weight: 0.1 * 100,
+        description: "lorem ipsum",
+      },
+    },
+    leftCase: [0, 0, 255],
+    rightCase: "white",
+    value: 0.05,
+  },
+  {
+    attribute: {
+      name: "manufacturer2",
+      type: "Taxonomy",
+      similarityConfiguration: {
+        simFunction: "lorem ipsum",
+        weight: 0.1 * 100,
+        description: "lorem ipsum",
+      },
+    },
+    leftCase: "ford",
+    rightCase: "subaru",
+    value: 0.05,
+  },
+  {
+    attribute: {
+      name: "make",
+      type: "nested",
+      similarityConfiguration: {
+        simFunction: "Nested similarity",
+        weight: 0.5 * 100,
+        description: "Weighted similarity of the nested attributes",
+      },
+    },
+    leftCase: "",
+    rightCase: "",
+    value: 0.12,
+    _children: [
+      {
         attribute: {
-            name: "make",
-            type: "nested",
-            similarityConfiguration: {
-                "simFunction": "Nested similarity",
-                "weight": 0.5*100,
-                "description": "Weighted similarity of the nested attributes"
-            },
+          name: "model",
+          type: "string",
+          similarityConfiguration: {
+            simFunction: "Levensthein",
+            weight: 0.3 * 100,
+            description: "String similarity using Levensthein",
+          },
         },
-        leftCase: "",
-        rightCase: "",
-        value: 0.12,
-        _children: [
-            {
-                attribute: {
-                    name: "model",
-                    type: "string",
-                    similarityConfiguration: {
-                        "simFunction": "Levensthein",
-                        "weight": 0.3*100,
-                        "description": "String similarity using Levensthein"
-                    },
-                },
-                leftCase: "silverado 2500 ltz",
-                rightCase: "outlander se",
-                value: 0.1,
-            },
-            {
-                attribute: {
-                    name: "manufacturer",
-                    type: "string",
-                    similarityConfiguration: {
-                        "simFunction": "Levensthein",
-                        "weight": 0.7*100,
-                        "description": "String similarity using Levensthein"
-                    },
-                },
-                leftCase: "chevrolet",
-                rightCase: "mitsubishi",
-                value: 0.27,
-            }              
-        ]
-    }
-]
+        leftCase: "silverado 2500 ltz",
+        rightCase: "outlander se",
+        value: 0.1,
+      },
+      {
+        attribute: {
+          name: "manufacturer",
+          type: "string",
+          similarityConfiguration: {
+            simFunction: "Levensthein",
+            weight: 0.7 * 100,
+            description: "String similarity using Levensthein",
+          },
+        },
+        leftCase: "chevrolet dsjkajdslakjdsla kjdlskaj dslkaj sdlkj ",
+        rightCase: "mitsubishi",
+        value: 0.27,
+      },
+    ],
+  },
+];
 window.addEventListener("load", (event) => { 
     Tabulator.registerModule([DataTreeModule, FormatModule]);
     let table = new Tabulator("#case-comparison-table", {
@@ -95,7 +139,7 @@ window.addEventListener("load", (event) => {
           formatterParams: {
             color: "#dda8f8",
             legendColor: "#000000",
-            legendAlign: "center",
+            legendAlign: "justify",
             legend: formatWeight,
           },
           cssClass: "dt-att-weight",
@@ -107,22 +151,38 @@ window.addEventListener("load", (event) => {
           formatter: renderSimilarityFunction,
         },
         {
-          columns: [
-            {
-              field: "leftCase",
-              hozAlign: "right",
-              formatter: formatByType,
-            },
-            {
-              field: "value",
-              width: "10%",
-              hozAlign: "center",
-              cssClass: "dt-att-value",
-            },
-            { field: "rightCase",formatter: formatByType },
-          ],
+          field: "leftCase",
+          hozAlign: "right",
+          formatter: formatByType,
+          cssClass: "dt-left-case",
+          headerHozAlign: "right",
+        },
+        {
+          field: "value",
+          width: "10%",
+          hozAlign: "center",
+          cssClass: "dt-att-value",
+          headerHozAlign: "center",
+        },
+        {
+          field: "rightCase",
+          formatter: formatByType,
+          cssClass: "dt-right-case",
         },
       ],
+    });
+
+    table.on("dataLoaded", function (data) {
+      console.log("data loaded");
+      let el = document.querySelector(".tabulator-col[role='columnheader'].dt-left-case .tabulator-col-title");
+      el.innerHTML = "Case Id";
+      el = document.querySelector(".tabulator-col[role='columnheader'].dt-right-case .tabulator-col-title");
+      el.innerHTML = "Case Id";
+      el = document.querySelector(
+        ".tabulator-col[role='columnheader'].dt-att-value .tabulator-col-title"
+      );
+      el.innerHTML = "Similarity";
+      
     });
 
 
@@ -131,26 +191,100 @@ window.addEventListener("load", (event) => {
 function formatByType(cell, formatterParams) {
     let data = cell.getData();
     if ("type" in data.attribute) {
-        switch(data.attribute["type"]) {
-            case "number":
-                return formatNumber(cell, formatterParams);
-                break;
-            case "nested":
-                return "&nbsp;";
-            default:
-                return formatDefault(cell);
+        switch (data.attribute["type"]) {
+          case "number":
+            return formatNumber(cell, formatterParams);
+            break;
+          case "Image":
+            return renderImage(cell, formatterParams);
+            break;
+          case "Color":
+            return renderColor(cell, formatterParams);
+            break;
+          case "ColorRGBList":
+            return renderColor(cell, formatterParams);
+            break;
+          case "Taxonomy":
+            return renderTaxonomyLabel(cell, formatterParams);
+            break;
+          case "nested":
+            return "&nbsp;";
+          default:
+            return formatDefault(cell);
         }
     } else {
         return formatDefault(cell);
     }
 }
 
+
+function renderImage(cell, formatterParams, onRendered) {
+    let data = cell.getData();
+    let template = document.createElement("template");
+    if (data && data[cell.getField()]) {
+      let value = data[cell.getField()];
+      template.innerHTML = `<img class="img-fluid" src="${value}"/>`;
+    } else {
+      template.innerHTML = `<span class="badge text-bg-danger">EMPTY</span>`;
+    }
+    return template.content.firstChild;    
+}
+
+function renderColor(cell, formatterParams, onRendered) {
+  let data = cell.getData();
+  let template = document.createElement("template");
+  if (data && data[cell.getField()]) {
+    let value = data[cell.getField()];
+    if (typeof(value) === "string") {
+        template.innerHTML = `<div class="box" aria-labelledby="color-label" title=${value} style="background-color: ${value};"></div>`;
+    } 
+    if (Array.isArray(value) && typeof(value[0]) === "number")  {
+        template.innerHTML = `<div class="box" aria-labelledby="color-label" title=rgb(${value}) style="background-color: rgb(${value});"></div>`;
+    }
+    if (Array.isArray(value) && Array.isArray(value[0])) {
+        let colorBoxes = value.map(
+            (value) =>
+            `<div class="box" aria-labelledby="color-label" title=rgb(${value}) style="background-color: rgb(${value});"></div>`
+        );
+        console.log(colorBoxes.join(""));
+        //template.innerHTML = `<div>colorBoxes.join("");
+        template.innerHTML = `<div>${colorBoxes.join("")}</div>`;
+    }
+  } else {
+    template.innerHTML = `<span class="badge text-bg-danger">EMPTY</span>`;
+  }
+  return template.content.firstChild;
+}
+
+function renderTaxonomyLabel(cell, formatterParams, onRendered) {
+  let data = cell.getData();
+  let template = document.createElement("template");
+  if (data && data[cell.getField()]) {
+    let value = data[cell.getField()];
+    template.innerHTML = `<a class="taxonomy-label" href="#" data-label="${value}">${value}</a>`;
+  } else {
+    template.innerHTML = `<span class="badge text-bg-danger">EMPTY</span>`;
+  }
+  let link = template.content.firstChild;
+  link.addEventListener("click", (event) => {
+        if (event.currentTarget){
+            let label = (event.currentTarget).getAttribute("data-label");
+            if (label) {
+                /// TODO
+                console.log(label)
+            }
+        }
+    })
+  return link ;
+}
+
+
 function formatDefault(cell) {
     if (cell.getValue()) {
         cell.getElement().style.whiteSpace = "pre-wrap";
         return cell.getValue();
     } else {
-        return `<span class="badge text-bg-danger">NULL</span>`;
+        return `<span class="badge text-bg-danger">EMPTY</span>`;
     }
 }
 
